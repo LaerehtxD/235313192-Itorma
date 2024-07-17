d884a439246b37067eea81a41af8b06bf2efa44a
LaerehtxD <itormadaryl2004@gmail.com>
2024-07-17 20:41:54 +0800

    batfile that does some functions

---
+++ BSCRIPT01.bat
@@ -0,0 +1,26 @@
+@echo off
+
+rem To open websites
+start chrome "https://www.facebook.com/"
+start chrome "https://leonardreyes.notion.site/"
+start chrome "https://www.youtube.com/"
+start chrome "https://tetr.io/"
+start chrome "https://uc-bcf.instructure.com/"
+
+pause
+
+rem To open calculator
+start calc.exe
+
+pause
+
+rem To open notepad
+start notepad.exe
+
+pause
+
+rem Initiate shutdown
+echo System shutting down in 10 seconds
+shutdown /s /t 10 
+
+
